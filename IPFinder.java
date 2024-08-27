6b04727f437abe4c7c6135ef3518a4e13d70c99f
kirankandel <kirankandel007@gmail.com>
2024-08-27 11:48:33 +0545

    refactor ipfinder

--- IPFinder.java
+++ IPFinder.java
@@ -1,24 +1,21 @@
 import java.net.*;
 import java.util.*;
+
 public class IPFinder {
-		public static void main(String args[]){
-			String host;
-			Scanner input = new Scanner(System.in);
-			InetAddress address;
-			System.out.println("\nEnter The Hostname");
-			host = input.next();
-			
-//As the class InetAddress throws UnknownHostExcepton, we try and catch the error 
-			try{
-				address = InetAddress.getByName(host);
-				System.out.println("\n\nThe IP Address is :  " + address.toString());
-			
-			}
-			
-			catch(UnknownHostException uhEx){
-				System.out.println("Could not find "+host);
-			}
-			
-			
+	public static void main(String args[]) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("\nEnter The Hostname");
+		String host = input.next();
+
+		try {
+			InetAddress address = InetAddress.getByName(host);
+			System.out.println("\n\nThe IP Address is :  " + address.toString());
+
+		} catch (UnknownHostException uhEx) {
+			System.out.println("Could not find " + host);
+		} finally {
+			input.close();
 		}
+
+	}
 }
